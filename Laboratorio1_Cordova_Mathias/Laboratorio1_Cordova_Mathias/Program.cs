008eb536d1c3a4fd873b58b67224b139f9b1817d
mathiascordova <macordova@miuandes.cl>
2020-03-17 18:29:52 -0300

    Clase 'Persona' creada

---
+++ Laboratorio1_Cordova_Mathias/Laboratorio1_Cordova_Mathias/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Laboratorio1_Cordova_Mathias
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona persona=new Persona();
+
+        }
+    }
+}
